395ab29bfc16a1a6e3ede594e7a92a0fafa035de
GinOwO <65128789+GinOwO@users.noreply.github.com>
2025-12-05 10:59:53 +0530

    Cleaned up day 5, gonna add a preprocess fn

--- 5.py
+++ 5.py
@@ -1,22 +1,13 @@
 import re
 import sys
-from typing import List
+from typing import List, Any
 
 
-def solveA(input: List[str]):
-    intervals = []
-
-    i = 0
-    while input[i] != "":
-        x, y = input[i].split("-")
-        intervals.append((int(x), int(y)))
-        i += 1
+def solveA(input: List[Any]):
+    intervals, nums = input
 
     ans = 0
-    i += 1
-
-    while i < len(input):
-        z = int(input[i])
+    for z in nums:
         found = False
         for x, y in intervals:
             if x <= z <= y:
@@ -28,7 +19,13 @@ def solveA(input: List[str]):
     print(ans)
 
 
-def solveB(input: List[str]):
+def solveB(input: List[Any]):
+    merged, _ = input
+    total = sum(y - x + 1 for x, y in merged)
+    print(total)
+
+
+def preprocess(input: List[str]):
     intervals = []
     i = 0
 
@@ -36,22 +33,26 @@ def solveB(input: List[str]):
         x, y = input[i].split("-")
         intervals.append([int(x), int(y)])
         i += 1
+    i += 1
 
     intervals.sort()
-    merged = [intervals[0]]
 
+    merged = [intervals[0]]
     for q in range(1, len(intervals)):
         if intervals[q][0] <= merged[-1][1]:
             merged[-1][1] = max(merged[-1][1], intervals[q][1])
         else:
             merged.append(intervals[q])
 
-    total = sum(y - x + 1 for x, y in merged)
-    print(total)
+    nums = []
+    for i in range(i, len(input)):
+        nums.append(int(input[i]))
+
+    return (merged, nums)
 
 
 if __name__ == "__main__":
-    inp = [i for i in sys.stdin.read().splitlines()]
+    inp = preprocess([i for i in sys.stdin.read().splitlines()])
     solveA(inp)
     print()
     solveB(inp)
